57a2890a7fe9a1fe7f8787cf64d0b3a82b0b23c0
Louis Matthijssen <l.matthijssen@yipyip.nl>
2023-05-04 13:48:19 +0200

    Include original error in SessionStorageError

--- lib/charon/session_plugs/session_storage_error.ex
+++ lib/charon/session_plugs/session_storage_error.ex
@@ -2,7 +2,9 @@ defmodule Charon.SessionPlugs.SessionStorageError do
   @moduledoc """
   Error raised when a new/updated session could not be stored.
   """
-  defexception message:
-                 "An error occurred when storing a new/updated session. The operation was cancelled.",
-               plug_status: 500
+  defexception [
+    :error,
+    message: "An error occurred when storing a new/updated session. The operation was cancelled.",
+    plug_status: 500
+  ]
 end
