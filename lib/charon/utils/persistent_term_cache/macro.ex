59dc31934db84c86abe64ce77233f0970b79f74a
Julien Smeets <16883019+juulSme@users.noreply.github.com>
2026-03-24 19:48:28 +0100

    Fix macro hygiene

--- lib/charon/utils/persistent_term_cache/macro.ex
+++ lib/charon/utils/persistent_term_cache/macro.ex
@@ -3,10 +3,8 @@ defmodule Charon.Utils.PersistentTermCache.Macro do
 
   defmacro get_or_create(key, do: create_block) do
     quote do
-      key = unquote(key)
-
-      case :persistent_term.get(key, nil) do
-        nil -> unquote(create_block) |> tap(&:persistent_term.put(key, &1))
+      case :persistent_term.get(unquote(key), nil) do
+        nil -> unquote(create_block) |> tap(&:persistent_term.put(unquote(key), &1))
         cached -> cached
       end
     end
